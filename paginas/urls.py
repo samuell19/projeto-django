238ff5ecee08a78425ed3d26dde6f0c3de23d6a7
samuell19 <samuelmedeiros2025@gmail.com>
2024-10-14 09:45:03 -0300

    first

---
+++ paginas/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import IndexView
+
+urlpatterns = [
+    #path('endereço'/, MinhaView.as_view(),name='nome da url'),
+    path('', IndexView.as_view(), name = 'inicio'),
+
+]
