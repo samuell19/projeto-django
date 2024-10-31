791530d3c70f72264f9066470de437f1840edfce
samuell19 <samuelmedeiros2025@gmail.com>
2024-10-31 11:03:17 -0300

    resolvido o problema de logout mas o rodape ficou desalinhado

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views 
+from .views import PerfilView
+
+
 
 urlpatterns=[
     #path('endereço'/, MinhaView.as_view(),name='nome da url'),
     path('login/', auth_views.LoginView.as_view(
         template_name='usuarios/form.html'
         ),name="login"),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', views.logout_view, name='logout'),
+    path('perfil/', PerfilView.as_view(), name='perfil'),
+
 
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 # app/views.py
+from django.views.generic import TemplateView
 
 from django.contrib.auth import logout
 from django.shortcuts import redirect
@@ -10,5 +11,8 @@ from django.views.decorators.http import require_POST
 def logout_view(request):
     logout(request)
     return redirect('inicio')
+
+class PerfilView(TemplateView):
+    template_name="usuarios/perfil.html"
 
 # Create your views here.
